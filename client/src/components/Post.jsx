d11ce82cb38fc48ba09027ac2e2cea5b597257f8
Andresr35 <andresjruiz@cpp.edu>
2024-01-06 17:57:36 -0800

    add handleLike function for posts in client

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -16,7 +16,8 @@ import { useNavigate } from "react-router-dom";
 
 const Post = ({ post, setNewPost, url }) => {
   const navigate = useNavigate();
-  const [error, setError] = useState("");
+  const [addCommentError, setAddCommentError] = useState("");
+  const [handleLikeError, setHandleLikeError] = useState("");
   const [openComments, setOpenComments] = useState(false);
   const handleComment = setOpenComments(!openComments);
 
@@ -35,9 +36,26 @@ const Post = ({ post, setNewPost, url }) => {
     });
     if (addCommentRes.status == 401) return navigate("/login");
     const addCommentData = await addCommentRes.json();
-    if (addCommentData.status == 201) {
-      // start doing stuff
-    } else setError(addCommentData.message);
+    if (addCommentData.status == 201) setNewPost(addCommentData.newPost);
+    else setAddCommentError(addCommentData.message);
+  };
+
+  const handleLike = async (e) => {
+    e.preventDefault();
+    const likeRes = await fetch(`${url}/api/posts/${post._id}/likes`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userID: localStorage.getItem("userID"),
+      }),
+    });
+    if (likeRes.status == 401) return navigate("/login");
+    const likeData = await likeRes.json();
+    if (likeData.status == 200) setNewPost(likeData.newPost);
+    else setHandleLikeError(likeData.message);
   };
 
   return (
@@ -46,7 +64,9 @@ const Post = ({ post, setNewPost, url }) => {
       <hr />
       <p>{post.message}</p>
       <p className={styles.timestamp}>Created: {post.timestamp}</p>
-      <p>Likes: {post.likes.length}</p>
+      {!handleLikeError.length == 0 && <p>{handleLikeError}</p>}
+
+      <p onClick={handleLike}>Likes: {post.likes.length}</p>
       <button onClick={handleComment}>Open Comments</button>
       {openComments && (
         <div className={styles.commentsContainer}>
@@ -54,7 +74,7 @@ const Post = ({ post, setNewPost, url }) => {
             <Comment comment={comment} key={index} setPost={setNewPost} />
           ))}
           <form className={styles.addComment} onSubmit={addComment}>
-            {!error.length == 0 && <p>{error}</p>}
+            {!addCommentError.length == 0 && <p>{addCommentError}</p>}
             <input type="text" name="comment" placeholder="Add Comment" />
             <button className={styles.add} type="submit">
               <img src="../../checkmark-svgrepo-com.svg" alt="Delete Button" />
